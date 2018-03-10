ea51013b3017fb154546fa70afe7ddd91eb25f40
Amit Wadhwa <amit.wadhwa1904@gmail.com>
2018-03-10 15:24:48 +1100

    Added code for speech to text converter

---
+++ MyApplication/app/src/main/java/com/example/ridhi/myapplication/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.ridhi.myapplication;
+
+import android.content.Intent;
+import android.speech.RecognizerIntent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvResult;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tvResult = (TextView)findViewById(R.id.TVResult);
+    }
+
+    public void onButtonClick(View view){
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+
+        if(intent.resolveActivity(getPackageManager()) != null){
+            startActivityForResult(intent, 10);
+        } else {
+            Toast.makeText(this, "Your device does not support Speech Input", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode){
+            case 10:
+                if(resultCode == RESULT_OK && data != null){
+                    ArrayList<String> list =  data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    tvResult.setText(list.get(0));
+                }
+                break;
+        }
+
+    }
+}
